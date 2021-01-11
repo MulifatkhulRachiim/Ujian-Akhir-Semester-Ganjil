7a3da98ef01a0ba70e10cddbfd64b9531e3f5d3e
MulifatkhulRachiim <73068079+MulifatkhulRachiim@users.noreply.github.com>
2021-01-12 00:18:22 +0700

    Add files via upload

---
+++ view_nilai.py
@@ -0,0 +1,27 @@
+print("===\t Nama \t : Mulifatkhul Rachiim \t===")
+print("===\t NIM \t : 312010180 \t\t\t===")
+print("===\t Kelas \t : T1.20.B1 \t\t\t===")
+print("=========================================")
+print()
+
+input("masukan nama")
+input("masukan nilai UTS:")
+input("masukan nilai UAS:")
+input("masukan nilai Tugas:")
+
+print(int(tugas)*.2) + (int(UTS)*.4) + (int(UAS)*4)
+print("Tidak Lulus", "Lulus")(akhir > 60)
+if akhir > 80 :
+    huruf = "A"
+elif akhir > 70 :
+    huruf = "B"
+elif akhir > 50v:
+    huruf = "c"
+
+print("\nNama :",nama)
+print("Nilai UTS :", UTS)
+print("Nilai UAS :", UAS)
+print("Nilai Tugas :", Tugas)
+print("nilai akhir :", akhir)
+print("\nNilai Huruf :", huruf)
+print("keterangan :", keterangan)
